e88ca1b862497b0524585a3b6a3441fdb1345991
wbadmin <>
2019-05-27 18:47:57 +0000

    {/src/main/java/com/QualityDefects.java}

--- src/main/java/com/QualityDefects.java
+++ src/main/java/com/QualityDefects.java
@@ -6,12 +6,24 @@ package com;
 
 public class QualityDefects implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public QualityDefects() {
-    }
+	@org.kie.api.definition.type.Label(value = "Defects")
+	private java.util.List<com.QualityDefect> defects;
 
+	public QualityDefects() {
+	}
 
+	public java.util.List<com.QualityDefect> getDefects() {
+		return this.defects;
+	}
 
+	public void setDefects(java.util.List<com.QualityDefect> defects) {
+		this.defects = defects;
+	}
+
+	public QualityDefects(java.util.List<com.QualityDefect> defects) {
+		this.defects = defects;
+	}
 
 }
