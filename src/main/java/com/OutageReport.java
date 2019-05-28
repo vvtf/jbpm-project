c78444639c7ad18a1c8600f2be85a3419c61dc98
wbadmin <>
2019-05-28 13:26:59 +0000

    {/src/main/java/com/OutageReport.java}

--- src/main/java/com/OutageReport.java
+++ src/main/java/com/OutageReport.java
@@ -6,12 +6,47 @@ package com;
 
 public class OutageReport implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public OutageReport() {
-    }
+	@org.kie.api.definition.type.Label(value = "Outage ID")
+	private java.lang.Long id;
+	@org.kie.api.definition.type.Label(value = "Outage description")
+	private java.lang.String description;
+	@org.kie.api.definition.type.Label(value = "Resolution description")
+	private java.lang.String resolution;
 
+	public OutageReport() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public java.lang.String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(java.lang.String description) {
+		this.description = description;
+	}
+
+	public java.lang.String getResolution() {
+		return this.resolution;
+	}
+
+	public void setResolution(java.lang.String resolution) {
+		this.resolution = resolution;
+	}
+
+	public OutageReport(java.lang.Long id, java.lang.String description,
+			java.lang.String resolution) {
+		this.id = id;
+		this.description = description;
+		this.resolution = resolution;
+	}
 
 }
